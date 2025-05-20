70fbef6222e5925e9a329ce91552e5e2f31ba099
s-renren <s1f102301156@iniad.org>
2025-05-20 23:59:04 +0900

    worksに説明を追加。クリックした際のポップアップで表示できるように

--- src/component/worksDetail/WorksDetail.tsx
+++ src/component/worksDetail/WorksDetail.tsx
@@ -5,16 +5,16 @@ import threeD_navi_pic from "@/assets/works/3dnavi.png";
 import hanabi_pic from "@/assets/works/hanabi.png";
 
 const works = [
-  { title: "オセロ", image: othello_pic },
-  { title: "マインスイーパー", image: minesweeper_pic },
-  { title: "草チェッカー", image: discordBot_pic },
-  { title: "3Dナビ", image: threeD_navi_pic },
-  { title: "花火大会オンライン", image: hanabi_pic },
+  { id:1, title: "オセロ", image: othello_pic, description: "モダンなオセロアプリ。自分が初めて作った作品。" },
+  { id:2, title: "マインスイーパー", image: minesweeper_pic, description: "モダンなマインスイーパーアプリ。難易度などを設定可能。" },
+  { id:3, title: "草チェッカー", image: discordBot_pic, description: "Discordのbot。草の数を取得し、生えていなかったらメッセージを送信してくれるようなアプリ。" },
+  { id:4, title: "3Dナビ", image: threeD_navi_pic, description: "2024技育キャンプハッカソン vol.9最優秀賞受賞。 旅先へのルートを3Dで見れるアプリ。" },
+  { id:5, title: "花火大会オンライン", image: hanabi_pic, description: "2024技育キャンプハッカソン vol.14最優秀賞受賞。 花火をオンラインで友達と楽しむことを目的としたアプリ。" },
 ];
 
 const WorksDetail = () => {
   return (
-    <div className="px-6 py-8">
+    <div className="px-6 py-8 z-50">
       <h1 className="text-3xl font-bold mb-6">Works</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {works.map((work, index) => (
